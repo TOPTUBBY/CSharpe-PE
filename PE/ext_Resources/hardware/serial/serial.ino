1ffe0b0a4b23089b15116814412be8e679c30013
Patiphan Phakdeeburi <Panther_05@windowslive.com>
2021-10-14 12:30:47 +0700

    Update serial.ino
    - Add interval time 10 sec to turn off DC-Source

--- PE/ext_Resources/hardware/serial/serial.ino
+++ PE/ext_Resources/hardware/serial/serial.ino
@@ -3,9 +3,12 @@ bool button_status ;
 bool button_status_1;
 int count;
 int count_1;
+int count_1_interrupt;
 String c = "";
 String d = "";
 String command = "";
+unsigned long previousMillis = 0;
+const long interval = 10000;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,9 +17,22 @@ void setup() {
   Serial1.begin(9600);
 
 }
+
 //Serial is GUI
 //Serial1 is Ext.Equipment
 void loop() {
+  //interrupt 10 sec DC off
+  unsigned long currentMillis = millis();
+  if  (count_1_interrupt == 1 && (currentMillis - previousMillis >= interval)) {
+    Serial.println("0");
+    Serial1.println("CONFigure:OUTPut OFF");
+    delay(100);
+    Serial1.println("CONFigure:OUTPut OFF");
+    count_1 = 0;
+    count = 0;
+    previousMillis = currentMillis;
+  }
+
   //ON-OFF(Foot button)---------------------------------
   int SW_INPUT = digitalRead(SW_INPUTPIN);
   if (SW_INPUT == LOW) {
@@ -36,12 +52,14 @@ void loop() {
     }
   }
   if (count_1 == 1) {
+    count_1_interrupt = 1;
     Serial.println("1");
     Serial1.println("CONFigure:OUTPut ON");
     delay(100);
     Serial1.println("CONFigure:OUTPut ON");
     count_1 = 0;
   }
+
   else if (count_1 == 2) {
     Serial.println("0");
     Serial1.println("CONFigure:OUTPut OFF");
@@ -49,6 +67,7 @@ void loop() {
     Serial1.println("CONFigure:OUTPut OFF");
     count_1 = 0;
     count = 0;
+    previousMillis = currentMillis;
   }
   //ON-OFF(GUI)---------------------------------
   if (Serial.available() > 0) {
@@ -79,7 +98,8 @@ void loop() {
       //delay(500);
       Serial.println(d);
       Serial1.println("*cls");
-      
+
     }
   }
+
 }
