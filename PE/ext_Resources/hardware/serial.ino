a7d43d601aaa71f0d4f1a76612c2b0d67cd44aab
Patiphan Phakdeeburi <Panther_05@windowslive.com>
2024-01-13 08:58:08 +0700

    Update serial.ino
    Update arduino code for support serial1 in Uno by use softwareSerial

--- PE/ext_Resources/hardware/serial.ino
+++ PE/ext_Resources/hardware/serial.ino
@@ -1,3 +1,6 @@
+#include <SoftwareSerial.h>
+#define rxPin 3
+#define txPin 4
 #define SW_INPUTPIN 2
 bool button_status ;
 bool button_status_1;
@@ -10,24 +13,29 @@ String command = "";
 unsigned long previousMillis = 0;
 const long interval = 10000;
 
+SoftwareSerial serial1 = SoftwareSerial(rxPin, txPin);
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
   pinMode(SW_INPUTPIN, INPUT_PULLUP);
   Serial.begin(9600);
-  Serial1.begin(9600);
+  serial1.begin(9600);
 
 }
 
 //Serial is GUI
-//Serial1 is Ext.Equipment
+//Serial1 (for Mega) is Ext.Equipment
+//serial1 (for Uno) is Ext.Equipment
 void loop() {
   //interrupt 10 sec DC off
   unsigned long currentMillis = millis();
   if  (count_1_interrupt == 1 && (currentMillis - previousMillis >= interval)) {
     Serial.println("0");
-    Serial1.println("CONFigure:OUTPut OFF");
+    serial1.println("CONFigure:OUTPut OFF");
     delay(100);
-    Serial1.println("CONFigure:OUTPut OFF");
+    serial1.println("CONFigure:OUTPut OFF");
     count_1 = 0;
     count = 0;
     previousMillis = currentMillis;
@@ -54,17 +62,17 @@ void loop() {
   if (count_1 == 1) {
     count_1_interrupt = 1;
     Serial.println("1");
-    Serial1.println("CONFigure:OUTPut ON");
+    serial1.println("CONFigure:OUTPut ON");
     delay(100);
-    Serial1.println("CONFigure:OUTPut ON");
+    serial1.println("CONFigure:OUTPut ON");
     count_1 = 0;
   }
 
   else if (count_1 == 2) {
     Serial.println("0");
-    Serial1.println("CONFigure:OUTPut OFF");
+    serial1.println("CONFigure:OUTPut OFF");
     delay(100);
-    Serial1.println("CONFigure:OUTPut OFF");
+    serial1.println("CONFigure:OUTPut OFF");
     count_1 = 0;
     count = 0;
     previousMillis = currentMillis;
@@ -73,31 +81,31 @@ void loop() {
   if (Serial.available() > 0) {
     c = Serial.readString();
     if (c == "1") {
-      Serial1.println("CONFigure:OUTPut ON");
+      serial1.println("CONFigure:OUTPut ON");
       Serial.println("ON");
       delay(100);
-      Serial1.println("CONFigure:OUTPut ON");
+      serial1.println("CONFigure:OUTPut ON");
     } else if (c == "0") {
-      Serial1.println("CONFigure:OUTPut OFF");
+      serial1.println("CONFigure:OUTPut OFF");
       delay(100);
-      Serial1.println("CONFigure:OUTPut OFF");
+      serial1.println("CONFigure:OUTPut OFF");
     } else {
       //Set point------------------------------
       String setType = c.substring(0, 1);
       String setValue = c.substring(2, 5);  //recieve max 999
       if (setType == "v") {
-        Serial1.println("sour:volt " + setValue);
+        serial1.println("sour:volt " + setValue);
         delay(500);
       } else if (setType == "a") {
-        Serial1.println("SOURce:CURRent " + setValue);
+        serial1.println("SOURce:CURRent " + setValue);
         delay(500);
       }
       //send to DC directly--------------------
-      Serial1.println(c);
-      d = Serial1.readString();
+      serial1.println(c);
+      d = serial1.readString();
       //delay(500);
       Serial.println(d);
-      Serial1.println("*cls");
+      serial1.println("*cls");
 
     }
   }
