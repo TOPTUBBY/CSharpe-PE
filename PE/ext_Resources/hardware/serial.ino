8833dcbe7e9cc6c9d8699de29612f98c352b1770
Patiphan Phakdeeburi <32039449+TOPTUBBY@users.noreply.github.com>
2026-08-29 09:51:36 +0700

    Fix output timeout and serial command handling

--- PE/ext_Resources/hardware/serial.ino
+++ PE/ext_Resources/hardware/serial.ino
@@ -1,113 +1,141 @@
 #include <SoftwareSerial.h>
+
 #define rxPin 3
 #define txPin 4
 #define SW_INPUTPIN 2
-bool button_status ;
-bool button_status_1;
-int count;
-int count_1;
-int count_1_interrupt;
-String c = "";
-String d = "";
-String command = "";
-unsigned long previousMillis = 0;
-const long interval = 30000;
 
-SoftwareSerial serial1 = SoftwareSerial(rxPin, txPin);
+const unsigned long OUTPUT_TIMEOUT_MS = 30000UL;
+const unsigned long DEBOUNCE_MS = 50UL;
+
+SoftwareSerial serial1(rxPin, txPin);
+
+bool outputOn = false;
+bool buttonState = HIGH;
+bool lastButtonReading = HIGH;
+unsigned long outputOnStartedAt = 0;
+unsigned long lastDebounceAt = 0;
+
+void setOutput(bool turnOn) {
+  outputOn = turnOn;
+
+  if (turnOn) {
+    outputOnStartedAt = millis();
+    serial1.println("CONFigure:OUTPut ON");
+    delay(100);
+    serial1.println("CONFigure:OUTPut ON");
+    Serial.println("1");
+  } else {
+    serial1.println("CONFigure:OUTPut OFF");
+    delay(100);
+    serial1.println("CONFigure:OUTPut OFF");
+    Serial.println("0");
+  }
+}
+
+void handleFootButton(unsigned long currentMillis) {
+  bool reading = digitalRead(SW_INPUTPIN);
+
+  if (reading != lastButtonReading) {
+    lastDebounceAt = currentMillis;
+  }
+
+  if ((currentMillis - lastDebounceAt) >= DEBOUNCE_MS && reading != buttonState) {
+    buttonState = reading;
+
+    if (buttonState == LOW) {
+      setOutput(!outputOn);
+    }
+  }
+
+  lastButtonReading = reading;
+}
+
+void forwardEquipmentResponse() {
+  if (serial1.available() > 0) {
+    String response = serial1.readString();
+    response.trim();
+
+    if (response.length() > 0) {
+      Serial.println(response);
+    }
+  }
+}
+
+void handleGuiCommand() {
+  if (Serial.available() <= 0) {
+    return;
+  }
+
+  String command = Serial.readString();
+  command.trim();
+
+  if (command.length() == 0) {
+    return;
+  }
+
+  if (command == "1") {
+    setOutput(true);
+    return;
+  }
+
+  if (command == "0") {
+    setOutput(false);
+    return;
+  }
+
+  int separatorIndex = command.indexOf(',');
+  if (separatorIndex > 0) {
+    String setType = command.substring(0, separatorIndex);
+    String setValue = command.substring(separatorIndex + 1);
+    setType.trim();
+    setValue.trim();
+
+    if (setValue.length() == 0) {
+      Serial.println("ERR:EMPTY_SETPOINT");
+      return;
+    }
+
+    if (setType == "v") {
+      serial1.println("SOURce:VOLTage " + setValue);
+      delay(100);
+      serial1.println("*cls");
+      forwardEquipmentResponse();
+      return;
+    }
+
+    if (setType == "a") {
+      serial1.println("SOURce:CURRent " + setValue);
+      delay(100);
+      serial1.println("*cls");
+      forwardEquipmentResponse();
+      return;
+    }
+  }
+
+  // Forward supported SCPI commands entered from the manual command box.
+  serial1.println(command);
+  delay(100);
+  forwardEquipmentResponse();
+}
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   pinMode(SW_INPUTPIN, INPUT_PULLUP);
-  Serial.begin(9600);
-  serial1.begin(9600);
 
+  Serial.begin(9600);
+  Serial.setTimeout(100);
+  serial1.begin(9600);
+  serial1.setTimeout(250);
 }
 
-//Serial is GUI
-//Serial1 (for Mega) is Ext.Equipment
-//serial1 (for Uno) is Ext.Equipment
 void loop() {
-  //interrupt 100 sec DC off
   unsigned long currentMillis = millis();
-  if  (count_1_interrupt == 1 && (currentMillis - previousMillis >= interval)) {
-    Serial.println("0");
-    serial1.println("CONFigure:OUTPut OFF");
-    delay(100);
-    serial1.println("CONFigure:OUTPut OFF");
-    count_1 = 0;
-    count = 0;
-    previousMillis = currentMillis;
-  }
 
-  //ON-OFF(Foot button)---------------------------------
-  int SW_INPUT = digitalRead(SW_INPUTPIN);
-  if (SW_INPUT == LOW) {
-    while (button_status < 1) {
-      delay(200);
-      button_status++;
-      button_status_1 = 0;
-      count++;
-      count_1 = count;
-    }
-  }
-  else {
-    while (button_status_1 < 1) {
-      delay(200);
-      button_status = 0;
-      button_status_1++;
-    }
-  }
-  if (count_1 == 1) {
-    count_1_interrupt = 1;
-    Serial.println("1");
-    serial1.println("CONFigure:OUTPut ON");
-    delay(100);
-    serial1.println("CONFigure:OUTPut ON");
-    count_1 = 0;
-  }
+  handleFootButton(currentMillis);
+  handleGuiCommand();
 
-  else if (count_1 == 2) {
-    Serial.println("0");
-    serial1.println("CONFigure:OUTPut OFF");
-    delay(100);
-    serial1.println("CONFigure:OUTPut OFF");
-    count_1 = 0;
-    count = 0;
-    previousMillis = currentMillis;
+  if (outputOn && (currentMillis - outputOnStartedAt >= OUTPUT_TIMEOUT_MS)) {
+    setOutput(false);
   }
-  //ON-OFF(GUI)---------------------------------
-  if (Serial.available() > 0) {
-    c = Serial.readString();
-    if (c == "1") {
-      serial1.println("CONFigure:OUTPut ON");
-      Serial.println("ON");
-      delay(100);
-      serial1.println("CONFigure:OUTPut ON");
-    } else if (c == "0") {
-      serial1.println("CONFigure:OUTPut OFF");
-      delay(100);
-      serial1.println("CONFigure:OUTPut OFF");
-    } else {
-      //Set point------------------------------
-      String setType = c.substring(0, 1);
-      String setValue = c.substring(2, 5);  //recieve max 999
-      if (setType == "v") {
-        serial1.println("sour:volt " + setValue);
-        delay(500);
-      } else if (setType == "a") {
-        serial1.println("SOURce:CURRent " + setValue);
-        delay(500);
-      }
-      //send to DC directly--------------------
-      Serial.println(c);
-      d = serial1.readString();
-      //delay(500);
-      Serial.println(d);
-      serial1.println("*cls");
-
-    }
-  }
-
 }
